2a0da8c9400b6fc524cc1d10d6021a49d3808c13
Brendon Murphy <bmurphy@mac001ec21d5071.megapath.biz>
2010-01-20 14:05:32 -0800

    Removed views from themes, made a helper to put rand theme in layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def rand_theme
+    @rand_theme ||= rand(2).zero? ? "test_theme" : "default_theme"
+  end
 end
